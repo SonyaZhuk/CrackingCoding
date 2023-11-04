e9de5e7e81bca33a08b96cd2bcfa454214afa686
Sofya Zhuk <sonya.zhook@mail.ru>
2023-11-04 21:39:18 +0300

    LeetCode::100. Same Tree

---
+++ src/leetcode/easy/tree/SameTree.java
@@ -0,0 +1,35 @@
+package leetcode.easy.tree;
+
+/**
+ * {@link <a href="https://leetcode.com/problems/same-tree/">100. Same Tree</a>}.
+ */
+public class SameTree {
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+
+        if (p!= null && q != null)
+            return p.val == q.val
+                    && isSameTree(p.left, q.left)
+                    && isSameTree(p.right, q.right);
+
+        return false;
+    }
+}
