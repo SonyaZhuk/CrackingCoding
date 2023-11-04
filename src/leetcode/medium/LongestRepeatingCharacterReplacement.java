0f46ae70ddbf867c818a907818e6e62751fa81c3
Sofya Zhuk <sonya.zhook@mail.ru>
2023-11-04 20:23:41 +0300

    LeetCode::692. Top K Frequent Words

---
+++ src/leetcode/medium/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,11 @@
+package leetcode.medium;
+
+/**
+ * {@link <a href="https://leetcode.com/problems/longest-repeating-character-replacement/">424. Longest Repeating Character Replacement</a>}.
+ */
+public class LongestRepeatingCharacterReplacement {
+
+    public int characterReplacement(String s, int k) {
+        return 0;
+    }
+}
---
+++ src/leetcode/medium/TopKFrequentWords.java
@@ -0,0 +1,25 @@
+package leetcode.medium;
+
+import java.util.*;
+
+/**
+ * {@link <a href="https://leetcode.com/problems/top-k-frequent-words/">692. Top K Frequent Words</a>}.
+ */
+public class TopKFrequentWords {
+
+    public List<String> topKFrequent(String[] words, int k) {
+
+        final Map<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 1) + 1);
+        }
+
+        final List<String> res = new ArrayList<>(map.keySet());
+
+        Collections.sort(res, (e1, e2) -> map.get(e1).equals(map.get(e2)) ?
+                e1.compareTo(e2) : map.get(e2) - map.get(e1));
+
+        return res.subList(0, k);
+    }
+}
