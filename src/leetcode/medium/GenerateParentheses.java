1ec7d90a86caf6acaad64c1b30ea6d31492a7cb5
Sofya Zhuk <sonya.zhook@mail.ru>
2023-11-10 08:18:44 +0300

    LeetCode::22. Generate Parentheses

---
+++ src/leetcode/medium/GenerateParentheses.java
@@ -0,0 +1,51 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * {@link <a href="https://leetcode.com/problems/generate-parentheses/">22. Generate Parentheses</a>}.
+ */
+public class GenerateParentheses {
+    private List<String> res;
+
+    public List<String> generateParenthesis(int n) {
+        res = new ArrayList<>();
+
+        generateParenthesis(new StringBuilder(), n, n);
+
+        return res;
+    }
+    private void generateParenthesis(StringBuilder sb, int open, int close) {
+        if (open == 0 && close == 0) {
+            res.add(sb.toString());
+        }
+
+        if (open > close || open < 0) return;
+
+        generateParenthesis(sb.append('('), open - 1, close);
+        sb.deleteCharAt(sb.length() - 1);
+
+        generateParenthesis(sb.append(')'), open, close - 1);
+        sb.deleteCharAt(sb.length() - 1);
+    }
+
+    //n = 1, (),
+    //n = 2, ()(), (())
+    //n = 3, ((())), ()()(), (())(), ()(()), (()())
+    public void printBrackets(StringBuilder curr, int open, int close, int n) {
+
+        if (curr.length() == 2 * n) {
+            System.out.println(curr);
+            return;
+        }
+        if (open < n) {
+            curr.append("(");
+            printBrackets(curr, open + 1, close, n);
+        }
+        if (close < n) {
+            curr.append(")");
+            printBrackets(curr, open, close + 1, n);
+        }
+    }
+}
