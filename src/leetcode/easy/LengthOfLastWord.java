a4ee7fa8686949a13cd778178fbcb53b0a8ce721
Sofya Zhuk <sonya.zhook@mail.ru>
2023-11-10 07:30:38 +0300

    LeetCode::58. Length of Last Word

---
+++ src/leetcode/easy/LengthOfLastWord.java
@@ -0,0 +1,29 @@
+package leetcode.easy;
+
+/**
+ * {@link <a href="https://leetcode.com/problems/length-of-last-word/">58. Length of Last Word</a>}.
+ */
+public class LengthOfLastWord {
+
+    public int lengthOfLastWord(String s) {
+        if (s.isBlank()) return -1;
+
+        final String[] arr = s.split(" ");
+
+        return arr[arr.length - 1].length();
+    }
+
+    public int lengthOfLastWord1(String s) {
+        if (s.isBlank()) return -1;
+
+        final String s1 = s.trim();
+
+        int count = 0;
+        for (int i = s1.length() - 1; i >=0; i--) {
+            if (s1.charAt(i) != ' ') count++;
+            else return count;
+        }
+
+        return count;
+    }
+}
