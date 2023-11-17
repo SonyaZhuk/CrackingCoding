23b9211e53557545e75d1e22eb2951a938c96403
Sofya Zhuk <sonya.zhook@mail.ru>
2023-11-17 10:19:43 +0300

    LeetCode::14. Longest Common Prefix

---
+++ src/leetcode/easy/LongestCommonPrefix.java
@@ -0,0 +1,17 @@
+package leetcode.easy;
+
+/**
+ * {@link <a href="https://leetcode.com/problems/longest-common-prefix/">14. Longest Common Prefix</a>}.
+ */
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for (int index = 1; index < strs.length; index++) {
+            while (strs[index].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+}
