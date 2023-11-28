43f2b14f0fb6006930e666baded88b41b5ab8bee
Sofya Zhuk <sonya.zhook@mail.ru>
2023-11-28 10:50:42 +0300

    LeetCode::125. Valid Palindrome

---
+++ src/leetcode/easy/ValidPalindrome.java
@@ -0,0 +1,44 @@
+package leetcode.easy;
+
+/**
+ * {@link <a href="https://leetcode.com/problems/valid-palindrome/">125. Valid Palindrome</a>}.
+ */
+public class ValidPalindrome {
+
+    public boolean isPalindrome(String s) {
+        final String s1 = s.toLowerCase().replaceAll("[^a-z\\d]", "");
+
+        if (s1.length() < 2) return true;
+
+        for (int i = 0; i < s1.length() / 2; i++) {
+            if (s1.charAt(i) != s1.charAt(s1.length() - 1 - i)) return false;
+        }
+        return true;
+    }
+
+    public boolean isPalindromeI(String s) {
+        if (s.isEmpty()) return true;
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+            } else {
+                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+
+        return true;
+    }
+}
