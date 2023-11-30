f6570f9334ffeb78b0ab6e6e0cf53823a658f1e5
Sofya Zhuk <sonya.zhook@mail.ru>
2023-11-30 10:43:43 +0300

    LeetCode::151. Reverse Words in a String

---
+++ src/leetcode/medium/ReverseWordsInAString.java
@@ -0,0 +1,19 @@
+package leetcode.medium;
+
+/**
+ * {@link <a href="https://leetcode.com/problems/reverse-words-in-a-string/">151. Reverse Words in a String</a>}.
+ */
+public class ReverseWordsInAString {
+    public String reverseWords(String s) {
+
+        final String[] arr = s.trim().split("\\s+");
+
+        final StringBuilder builder = new StringBuilder(arr[arr.length - 1]);
+        for (int i = arr.length - 2; i >= 0 ; i--) {
+            builder.append(" ");
+            builder.append(arr[i]);
+        }
+
+        return builder.toString();
+    }
+}
