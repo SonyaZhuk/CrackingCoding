81fc9ff04c7bc1f5cebe483f5518e15056c2cee1
Sofya Zhuk <sonya.zhook@mail.ru>
2023-12-27 16:23:05 +0300

    LeetCode::110. Balanced Binary Tree

--- src/leetcode/easy/tree/BalancedBinaryTree.java
+++ src/leetcode/easy/tree/BalancedBinaryTree.java
@@ -1,7 +1,7 @@
 package leetcode.easy.tree;
 
 /**
- * {@link <a href="https://leetcode.com/problems/path-sum/description/">112. Path Sum</a>}.
+ * {@link <a href="https://leetcode.com/problems/balanced-binary-tree/">110. Balanced Binary Tree</a>}.
  */
 public class BalancedBinaryTree {
     public class TreeNode {
@@ -27,6 +27,7 @@ public class BalancedBinaryTree {
         if (root == null) return true;
         return heightRec(root) != -1;
     }
+
     private int heightRec(TreeNode root) {
         if (root == null) return 0;
 
@@ -39,5 +40,23 @@ public class BalancedBinaryTree {
         if (Math.abs(leftHeight - rightHeight) > 1) return -1;
 
         return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    public boolean isBalancedI(TreeNode root) {
+        if (root == null) return true;
+
+        if (Math.abs(height(root.left) - height(root.right)) > 1) return false;
+
+        return isBalancedI(root.left) && isBalancedI(root.right);
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) return 0;
+
+        //we are adding 1 here, because we have to consider the root also
+        int lh = 1 + height(root.left);
+        int rh = 1 + height(root.right);
+
+        return Math.max(lh, rh);
     }
 }
