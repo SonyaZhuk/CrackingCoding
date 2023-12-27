197e120ab100f40d16538e0e99df22bd517c9363
Sofya Zhuk <sonya.zhook@mail.ru>
2023-12-27 16:57:34 +0300

    LeetCode::1324. Print Words Vertically

---
+++ src/leetcode/medium/PrintWordsVertically.java
@@ -0,0 +1,30 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * {@link <a href="https://leetcode.com/problems/print-words-vertically/">1324. Print Words Vertically</a>}.
+ */
+public class PrintWordsVertically {
+
+    public List<String> printVertically(String s) {
+        //"TO BE OR NOT TO BE"
+        List<String> res = new ArrayList<>();
+        String[] arr = s.split(" ");
+        int maxLen = 0;
+        for (String str: arr) {
+            if (str.length() > maxLen) maxLen = str.length();
+        }
+
+        for (int i = 0; i < maxLen; i++) {
+            StringBuilder builder = new StringBuilder();
+            for (String str: arr) {
+                char ch = (str.length() > i) ? str.charAt(i) : ' ';
+                builder.append(ch);
+            }
+            res.add(builder.toString().stripTrailing());
+        }
+        return res;
+    }
+}
