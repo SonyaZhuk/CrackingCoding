087afb6a9f782cdf061d490a921281c813de84db
Sofya Zhuk <sonya.zhook@mail.ru>
2023-12-27 16:07:38 +0300

    LeetCode::20. Valid Parentheses

--- src/leetcode/easy/ValidParentheses.java
+++ src/leetcode/easy/ValidParentheses.java
@@ -17,11 +17,38 @@ public class ValidParentheses {
                 stack.push('}');
             } else if (ch == '[') {
                 stack.push(']');
-            } else  {
+            } else {
                 if (stack.isEmpty() || stack.pop() != ch) return false;
             }
         }
 
         return stack.isEmpty();
+    }
+
+    public boolean isValidI(String s) {
+
+        char[] arr = s.toCharArray();
+
+        if (arr.length == 0 || arr.length % 2 != 0) return false;
+
+        for (int i = 0; i < arr.length - 1; i += 2) {
+            switch (arr[i]) {
+                case ('('):
+                    if (arr[i + 1] != ')') return false;
+                    else break;
+
+                case ('{'):
+                    if (arr[i + 1] != '}') return false;
+                    else break;
+
+                case ('['):
+                    if (arr[i + 1] != ']') return false;
+                    else break;
+
+                default:
+                    return false;
+            }
+        }
+        return true;
     }
 }
