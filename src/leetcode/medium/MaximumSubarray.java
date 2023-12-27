9fad7c06f723d0973953f4d2c4923cf630f2ca84
Sofya Zhuk <sonya.zhook@mail.ru>
2023-12-27 22:26:19 +0300

    LeetCode::53. Maximum Subarray

---
+++ src/leetcode/medium/MaximumSubarray.java
@@ -0,0 +1,23 @@
+package leetcode.medium;
+
+
+/**
+ * {@link <a href="https://leetcode.com/problems/maximum-subarray/">53. Maximum Subarray</a>}.
+ */
+public class MaximumSubarray {
+
+    //Kadane's Algorithm
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+        int max = Integer.MIN_VALUE, sum = 0;
+
+        for (int i = 0; i < len; i++) {
+            sum += nums[i];
+            max = Math.max(sum, max);
+
+            if (sum < 0) sum = 0;
+        }
+
+        return max;
+    }
+}
