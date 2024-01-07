d166445e1ccf78ee4c31a3cf104538efeef3c17f
Sofya Zhuk <sonya.zhook@mail.ru>
2024-01-07 17:51:19 +0300

    LeetCode::448. Find All Numbers Disappeared in an Array

---
+++ src/leetcode/easy/FindAllNumbersDisappearedinArray.java
@@ -0,0 +1,52 @@
+package leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * {@link <a href="https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/">448. Find All Numbers Disappeared in an Array</a>}.
+ */
+public class FindAllNumbersDisappearedinArray {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else i++;
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                ans.add(j + 1);
+            }
+
+        }
+        return ans;
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    public List<Integer> findDisappearedNumbersI(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int val : nums) {
+            set.add(val);
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = 1; i <= nums.length; i++) {
+            if (!set.contains(i)) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+}
