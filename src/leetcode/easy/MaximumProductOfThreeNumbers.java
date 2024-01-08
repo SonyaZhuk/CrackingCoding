9e13c880a39eb2699793c2ef71688330dca82bfe
Sofya Zhuk <sonya.zhook@mail.ru>
2024-01-08 20:09:45 +0300

    LeetCode::628. Maximum Product of Three Numbers

---
+++ src/leetcode/easy/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,44 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+
+/**
+ * {@link <a href="https://leetcode.com/problems/maximum-product-of-three-numbers/">628. Maximum Product of Three Numbers</a>}.
+ */
+public class MaximumProductOfThreeNumbers {
+
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length - 1;
+        int max1 = nums[len] * nums[len - 1] * nums[len - 2];
+        int max2 = nums[0] * nums [1] * nums[len];
+        return Math.max(max1, max2);
+    }
+
+    public int maximumProductI(int[] nums) {
+        int min1, min2, max1, max2, max3;
+        min1 = min2 = Integer.MAX_VALUE;
+        max1 = max2 = max3 = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            if(max1 < nums[i]){
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+            }else if(max2 < nums[i]){
+                max3 = max2;
+                max2 = nums[i];
+            }else if(max3 < nums[i]){
+                max3 = nums[i];
+            }
+
+            if(min1 > nums[i]){
+                min2 = min1;
+                min1 = nums[i];
+            }else if(min2 > nums[i]){
+                min2 = nums[i];
+            }
+
+        }
+        return Math.max(max1 * max2 * max3 , min1 * min2 * max1);
+    }
+}
