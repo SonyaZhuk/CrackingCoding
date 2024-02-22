2c27c389e365f3a8158991254078d3db1fec3f2e
Sofya Zhuk <sonya.zhook@mail.ru>
2024-02-22 10:15:15 +0300

    LeetCode::12. Integer to Roman

--- src/leetcode/medium/common/IntegerToRoman.java
+++ src/leetcode/medium/common/IntegerToRoman.java
@@ -1,7 +1,5 @@
 package leetcode.medium.common;
 
-import java.util.*;
-
 /**
  * {@link <a href="https://leetcode.com/problems/integer-to-roman/">12. Integer to Roman</a>}.
  */
@@ -16,29 +14,14 @@ public class IntegerToRoman {
     }
 
     public String intToRomanI(int num) {
-        final Map<Integer, String> map = new HashMap<>();
-        map.put(1, "I");
-        map.put(4, "IV");
-        map.put(5, "V");
-        map.put(9, "IX");
-        map.put(10, "X");
-        map.put(40, "XL");
-        map.put(50, "L");
-        map.put(90, "XC");
-        map.put(100, "C");
-        map.put(400, "CD");
-        map.put(500, "D");
-        map.put(900, "CM");
-        map.put(1000, "M");
-
-        final List<Integer> list = new ArrayList<>(map.keySet());
-        Collections.sort(list, Collections.reverseOrder());
+        final int[] val = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        final String[] rom = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
         final StringBuilder sb = new StringBuilder();
-        for (int n : list) {
-            while (num >= n) {
-                sb.append(map.get(n));
-                num -= n;
+        for(int i = 0; num > 0; i++) {
+            while (num >= val[i]) {
+                sb.append(rom[i]);
+                num -= val[i];
             }
         }
 
