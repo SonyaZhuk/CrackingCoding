eb3075f147378bd4d9c145b329f6004e9a60686a
Sofya Zhuk <sonya.zhook@mail.ru>
2024-03-06 09:39:24 +0300

    LeetCode::92. Reverse Linked List II

---
+++ src/leetcode/medium/list/ReverseLinkedListII.java
@@ -0,0 +1,46 @@
+package leetcode.medium.list;
+
+/**
+ * {@link <a href="https://leetcode.com/problems/reverse-linked-list-ii/">92. Reverse Linked List II</a>}.
+ */
+public class ReverseLinkedListII {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right) return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        for (int i = 0; i < left - 1; ++i) {
+            prev = prev.next;
+        }
+
+        ListNode current = prev.next;
+
+        for (int i = 0; i < right - left; ++i) {
+            ListNode nextNode = current.next;
+            current.next = nextNode.next;
+            nextNode.next = prev.next;
+            prev.next = nextNode;
+        }
+
+        return dummy.next;
+    }
+}
